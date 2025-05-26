d52762cab91c53644c2569d84d2fab97a364f000
soheil rahsaz <soheilrahsaz.sr@gmail.com>
2025-05-26 14:35:47 +0330

    reformated

--- src/main/resources/infra/docker-commands.sh
+++ src/main/resources/infra/docker-commands.sh
@@ -1,3 +1,1 @@
-docker network create ai-assist
-
-docker run -d --network ai-assist --name cassandra5.0.4  -v C:/other/cassandra:/var/lib/cassandra -p 9042:9042 cassandra:5.0.4
+docker run -d  --name cassandra5.0.4  -v C:/other/cassandra:/var/lib/cassandra -p 9042:9042 cassandra:5.0.4
